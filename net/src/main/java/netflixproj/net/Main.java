19731de0e147aaf6c4b12a9e4e231a520e044444
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-12-03 14:41:47 -0300

    aula10

--- net/src/main/java/netflixproj/net/Main.java
+++ net/src/main/java/netflixproj/net/Main.java
@@ -1,33 +1,48 @@
 package netflixproj.net;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Main implements Identificavel{
-	
+public class Main {
+
 	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("netflixproj");
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		Usuario p = new Usuario();
-		p.setId(1l);
-		p.setNome("joe");
-		em.persist(p);
-		em.getTransaction().commit();
-		em.close();
-		factory.close();
-	}
-
-	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setId(Long id) {
-		// TODO Auto-generated method stub
+		DAO<Usuario> dao = new DAO<Usuario>();
+		Usuario u = new Usuario();
+		u.setId(22l);
+		u.setNome("RibamarRS");
+		dao.save(u);
+		Usuario u2 = new Usuario();
+		u2.setId(23l);
+		u2.setNome("Ribamar");
+		dao.save(u2);
+		Usuario find = dao.find(Usuario.class, 22l);
+		System.out.println(find.getId());
 		
+		
+		DAO<Categorias> dao1 = new DAO<Categorias>();
+		Categorias c = new Categorias();
+		c.setId(22l);
+		c.setMelhores_filmes_avaliados("RibamarRS");
+		dao1.save(c);
+		Categorias c2 = new Categorias();
+		c2.setId(23l);
+		c2.setMelhores_filmes_avaliados("Ribamar");
+		dao1.save(c2);
+		Categorias find1 = dao1.find(Categorias.class, 22l);
+		System.out.println(find1.getId());
+		
+		DAO<Conteudo> dao = new DAO<Conteudo>();
+		Conteudo o = new Conteudo();
+		o.setId(22l);
+		o.setNome("RibamarRS");
+		dao.save(o);
+		Conteudo o2 = new Usuario();
+		o2.setId(23l);
+		o2.setNome("Ribamar");
+		dao.save(u2);
+		Usuario find = dao.find(Conteudo.class, 22l);
+		System.out.println(find.getId());
 	}
 	
-
+	
 }
